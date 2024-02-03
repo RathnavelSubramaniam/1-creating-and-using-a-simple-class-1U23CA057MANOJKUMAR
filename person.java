dff5580cbf7c4f746d8c1f23cd07a586bae8effa
1U23CA057MANOJKUMAR <139428849+1U23CA057MANOJKUMAR@users.noreply.github.com>
2024-02-03 08:52:23 +0000

    p1

--- person.java
+++ person.java
@@ -1,9 +1,9 @@
-class person
+class persona
 {
     String Firstname;
     String Lastname;
     int age;
-    person(String Fname,String Lname,int a)
+    persona(String Fname,String Lname,int a)
     {
         Firstname = Fname;
         Lastname = Lname;
@@ -17,8 +17,8 @@ class person
     class Person{
     public static void main(String args[])
     {
-        person a1 = new person("Jhon","doe",30);
-        person a2 = new person("Alice","Smith",25);
+        persona a1 = new persona("Jhon","doe",30);
+        persona a2 = new persona("Alice","Smith",25);
         System.out.println("person 1:" + a1.getfullname());
         System.out.println("person 2:" + a2.getfullname());//
         double average = ((a1.age + a2.age)/2.0);
