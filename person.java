020a041e86fde3bfcb0d26e8cfa0c87461715e0a
1U23CA057MANOJKUMAR <139428849+1U23CA057MANOJKUMAR@users.noreply.github.com>
2024-02-03 08:50:36 +0000

    P1

--- person.java
+++ person.java
@@ -20,9 +20,9 @@ class person
         person a1 = new person("Jhon","doe",30);
         person a2 = new person("Alice","Smith",25);
         System.out.println("person 1:" + a1.getfullname());
-        System.out.println("person 2:" + a2.getfullname());
+        System.out.println("person 2:" + a2.getfullname());//
         double average = ((a1.age + a2.age)/2.0);
         System.out.println("Average :" + average);
         
-    }
+    }//
 }
